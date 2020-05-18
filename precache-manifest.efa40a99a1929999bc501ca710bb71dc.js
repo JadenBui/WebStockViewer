b046a87d5f30a8c26c8e0435ee6b5cb3f1ffbbc4
JadenBui <anhbui11211@gmail.com>
2020-05-18 12:08:48 +1000

    Updates

---
+++ precache-manifest.efa40a99a1929999bc501ca710bb71dc.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4100ade66ab94a2c938013b4e20888",
+    "url": "/WebStockViewer/index.html"
+  },
+  {
+    "revision": "65e16b971b09b099ca4d",
+    "url": "/WebStockViewer/static/css/2.0f9e80ac.chunk.css"
+  },
+  {
+    "revision": "2b7578d3cbb66b8f2459",
+    "url": "/WebStockViewer/static/css/main.1020141a.chunk.css"
+  },
+  {
+    "revision": "65e16b971b09b099ca4d",
+    "url": "/WebStockViewer/static/js/2.95f3f529.chunk.js"
+  },
+  {
+    "revision": "0667c3fc742d395704647b73991e3960",
+    "url": "/WebStockViewer/static/js/2.95f3f529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7578d3cbb66b8f2459",
+    "url": "/WebStockViewer/static/js/main.7c4c6108.chunk.js"
+  },
+  {
+    "revision": "ac2eacac952ee568f8d6",
+    "url": "/WebStockViewer/static/js/runtime-main.1b1f5c02.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/WebStockViewer/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/WebStockViewer/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f5e164a3ec83d2e489340045d9367959",
+    "url": "/WebStockViewer/static/media/bground.f5e164a3.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/WebStockViewer/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/WebStockViewer/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/WebStockViewer/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/WebStockViewer/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/WebStockViewer/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a0c68086799ea6c3ea8e102004048813",
+    "url": "/WebStockViewer/static/media/data.a0c68086.jpeg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/WebStockViewer/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/WebStockViewer/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/WebStockViewer/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/WebStockViewer/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/WebStockViewer/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/WebStockViewer/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/WebStockViewer/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/WebStockViewer/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/WebStockViewer/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/WebStockViewer/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/WebStockViewer/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/WebStockViewer/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/WebStockViewer/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/WebStockViewer/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/WebStockViewer/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/WebStockViewer/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd1484b50947286f301a3419e1d6704c",
+    "url": "/WebStockViewer/static/media/homeBG.cd1484b5.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/WebStockViewer/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/WebStockViewer/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/WebStockViewer/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/WebStockViewer/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/WebStockViewer/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "fc676eab839cc2a8134ac511f47e2c39",
+    "url": "/WebStockViewer/static/media/loginBG.fc676eab.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/WebStockViewer/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/WebStockViewer/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/WebStockViewer/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/WebStockViewer/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/WebStockViewer/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "94a2c5e10e23619ad89ee0e0e117ea66",
+    "url": "/WebStockViewer/static/media/programming.94a2c5e1.jpeg"
+  },
+  {
+    "revision": "86d890bbb76ea4cbed9fbc5d264f996d",
+    "url": "/WebStockViewer/static/media/singleCard.86d890bb.jpg"
+  },
+  {
+    "revision": "dc034ef7d1ffe2a4644ba64a9137f946",
+    "url": "/WebStockViewer/static/media/stock-exchange.dc034ef7.jpg"
+  },
+  {
+    "revision": "e4b61b0fb8766f409c5420941ae1ccf4",
+    "url": "/WebStockViewer/static/media/stock.e4b61b0f.jpeg"
+  }
+]);
